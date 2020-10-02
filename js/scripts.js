0c3727de95c4ead1dfbc49181dba8b221896fdca
Oksi <ksibash93@gmail.com>
2020-10-02 22:55:27 +0300

    dialog animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
-$(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
-            },
-
-            1000:{
-                items:1
-            }
-        }
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        },
+        resizable: false,
     });
-});
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
